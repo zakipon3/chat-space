80f76547338a687233e28f5513f0889aca1d7d3f
zakipon3 <miyapon3@gmail.com>
2020-04-16 17:17:21 +0900

    message upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-
+  process resize_to_fit: [800, 800]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -43,7 +43,5 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
-  process resize_to_fit: [800, 800]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20200415025904) do
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "body",       limit: 65535
+    t.text     "content",    limit: 65535
     t.string   "image"
     t.integer  "group_id"
     t.integer  "user_id"
