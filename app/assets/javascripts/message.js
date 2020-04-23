50d2999f86052faf391fb5e9fbc693017cebe550
zakipon3 <miyapon3@gmail.com>
2020-04-23 15:04:04 +0900

    comment-out_delete

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -65,28 +65,20 @@ $(function(){
   })
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     var last_message_id = $('.message:last').data("message-id");
-    // console.log(last_message_id);
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
       if (messages.length !== 0) {
-      //追加するHTMLの入れ物を作る
       var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       $.each(messages, function(i, message) {
         insertHTML += buildHTML(message)
         console.log(insertHTML);
   });
-      //メッセージが入ったHTMLに、入れ物ごと追加
       $('.main__chatspace').append(insertHTML);
       $('.main__chatspace').animate({ scrollTop: $('.main__chatspace')[0].scrollHeight});
       }
@@ -95,9 +87,7 @@ $(function(){
       alert('error');
     });
   };
-  // //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
-
 });
